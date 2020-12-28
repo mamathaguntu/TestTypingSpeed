745464ed9820ae72ea245fd81947f4ec9e5b0328
mamathaguntu <mamathaguntu@gmail.com>
2020-12-27 19:11:53 -0800

    changes to get the typed text

--- script.js
+++ script.js
@@ -5,5 +5,18 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
+function spellCheck() {
+  let enteredTxt = testArea.nodeValue;
+  console.log(enteredTxt);
+}
+function start() {
+  let enteredTxtLen = testArea.nodeValue.length;
+  console.log(enteredTxtLen);
+}
+function reset() {
+  console.log("reset button has been pressed");
+}
+testArea.addEventListener("keypress", start, false);
+testArea.addEventListener("keyup", spellCheck, false);
 
-
+resetButton.addEventListener("click", reset, false);
